31f9c905c461fb69c55cd1b0aabfb3045366f79c
Jeremy Spradlin <Erbun@Jeremys-MacBook-Pro.local>
2017-10-05 16:09:21 -0400

    Added Text Field delegate and attributes

--- MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/ViewController.swift
@@ -11,27 +11,54 @@ import AVFoundation
 import Photos
 
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate{
     
     @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var albumButton: UIBarButtonItem!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
     
+//    let memeTextAttributes:[String:Any] = [
+//        NSStrokeColorAttributeName: UIColor.black,
+//        NSForegroundColorAttributeName: UIColor.white,
+//        NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+//        NSStrokeWidthAttributeName: -3.0]
+    
+    let memeTextAttributes:[String:Any] = [
+        NSAttributedStringKey.strokeColor.rawValue: UIColor.black,
+        NSAttributedStringKey.foregroundColor.rawValue: UIColor.white,
+        NSAttributedStringKey.font.rawValue: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedStringKey.strokeWidth.rawValue: -3.0]
+
         
     override func viewDidLoad() {
         super.viewDidLoad()
+        topTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.defaultTextAttributes = memeTextAttributes
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        topTextField.text = "TOP"
+        topTextField.textAlignment = .center
+        self.topTextField.delegate = self
+        bottomTextField.text = "BOTTOM"
+        bottomTextField.textAlignment = .center
+        bottomTextField.delegate = self
+        print("Inside viewDidLoad")
+        
+
+        
+
     }
     
     
-    //Action for when the 'pick' button is selected by the user
+    //User selects to pick an image from the album
     @IBAction func pickAnImageFromAlbum(_ sender: Any) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
         imagePicker.sourceType = .photoLibrary
         self.present(imagePicker, animated: true, completion: nil)
     }
-    
+    //User selects to take a picture with the camera
     @IBAction func pickAnImageFromCamera(_ sender: Any) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
@@ -43,15 +70,24 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     
     
-    
+    //Function for Image Picker delegate
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
         let image = info[UIImagePickerControllerOriginalImage] as? UIImage
         imagePickerView.image = image
         print("Inside imagePickerController")
         dismiss(animated: true, completion: nil)
-        
     }
 
+    //Functions for text field delegate
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("Inside textFieldDidBeginEditing")
+        textField.text = ""
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
+    
 }
 
