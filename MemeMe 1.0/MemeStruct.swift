690e69d7410b5dcd24f95fe870ca570ae6acd273
Jeremy Spradlin <spradlinjk@gmail.com>
2017-10-08 20:31:05 -0400

    Added Meme Struct File

---
+++ MemeMe 1.0/MemeStruct.swift
@@ -0,0 +1,17 @@
+//
+//  MemeStruct.swift
+//  MemeMe 1.0
+//
+//  Created by Jeremy Spradlin on 10/8/17.
+//  Copyright © 2017 Udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topTextField: String!
+    var bottomTextField: String!
+    var originalImage: UIImage!
+    var memedImage: UIImage!
+}
